f4b8952e80a4a3cd3e8ad4e29b765d7210bbe2a7
challengerST <18137279513@163.com>
2021-10-27 11:37:43 +0800

    <feature> (2) 修改readme文档

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
+<!--
+ * @Author: etong
+ * @Date: 2021-10-26 18:20:11
+ * @LastEditTime: 2021-10-27 11:25:38
+ * @LastEditors: your name
+ * @Description: 
+ * @FilePath: \jj-cli\README.md
+ * Think first, Program later
+-->
 # jj-cli
+
+https://img.shields.io/npm/v/@etongduomi/jj-cli
+
+https://img.shields.io/bundlephobia/min/@etongduomi/jj-cli
+
+## Install
+```
+$ npm i @etongduomi/jj-cli
+```
+
+## Usage
+```
+js
+
+const 
+```
--- bin/www/index.js
+++ bin/www/index.js
@@ -2,13 +2,14 @@
 /*
  * @Author: etong
  * @Date: 2021-10-26 11:28:19
- * @LastEditTime: 2021-10-26 15:14:44
+ * @LastEditTime: 2021-10-27 10:47:55
  * @LastEditors: your name
  * @Description: 
- * @FilePath: \clidemo\bin\www\index.js
+ * @FilePath: \jj-cli\bin\www\index.js
  * Think first, Program later
  */
-const testTiny = function(string) {
-    if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
-    return string.replace(/\s/g, "");
-}
+// const testTiny = function(string) {
+//     if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
+//     return string.replace(/\s/g, "");
+// }
+console.log('123456')
